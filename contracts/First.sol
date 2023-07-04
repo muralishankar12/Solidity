b7801e829ba7768ab410a115889382c5212d117f
muralishankar12 <muralishankar12@gmail.com>
2023-07-04 17:29:46 +0530

    new branch commit

--- contracts/First.sol
+++ contracts/First.sol
@@ -8,5 +8,5 @@ contract First{
     bool public value = true;
     bytes public name = "peter";
     string public name1 = "parker";
-    address public live = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public live = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; // address
 }
